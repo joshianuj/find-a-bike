aece58c039c1445a9b3e015492557fdf2f96e9d1
Anuj <anuj.joshi@candylabs.de>
2020-01-28 09:05:46 +0100

    Add initial repository files

---
+++ src/components/App/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './App.module.scss'
+
+const App = () => (
+    <div className={styles.app}>
+        <header className={styles.header}>Application Header</header>
+    </div>
+)
+
+export default App
---
+++ src/components/User/User.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './User.module.scss'
+
+const User = () => (
+    <div className={styles.User}>
+        <header className={styles.header}>Application Header</header>
+    </div>
+)
+
+export default User
