2690e0d4de37aa50060bf8cca1b40f0c8be702ad
joshianuj <joshi.anujo7@gmail.com>
2020-01-30 07:31:02 +0100

    Fix design and finally config file

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -10,16 +10,18 @@ export default () => (
   <IfFirebaseUnAuthed>
     {() => (
       <div className={styles.loginContainer}>
-        <h1>Hello, dear biker</h1>
-        <h3>Wanna ride!! Just hit the login button </h3>
-        <button
-          onClick={() => {
-            const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
-            firebase.auth().signInWithPopup(googleAuthProvider);
-          }}
-        >
-          Sign In with Google
-        </button>
+        <div className={styles.content}>
+          <h1>Hello, dear biker</h1>
+          <h3>Wanna ride!! Just hit the login button </h3>
+          <button
+            onClick={() => {
+              const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+              firebase.auth().signInWithPopup(googleAuthProvider);
+            }}
+          >
+            Sign In with Google
+          </button>
+        </div>
       </div>
     )}
   </IfFirebaseUnAuthed>
